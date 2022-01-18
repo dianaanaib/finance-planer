1d99af0377279b996723a08d40839a3e2d2bca2b
Diana Tungatarov <dianatungatarov@surfer-172-29-31-1-wlan-login.oszimt.de>
2022-01-18 08:19:28 +0100

    fill list comp with real data

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,41 +1,39 @@
 import React, { useState, useEffect } from 'react'
-import { List, Skeleton } from 'antd'
+import { Col, List, Row } from 'antd'
+
 import './index.css'
 
-const TransactionsList = () => {
-    const dummy = [
-        {
-            title: 'Title 1',
-            info: 'Info 1'
-          },
-          {
-            title: 'Title 2',
-            info: 'Info 2'
-          },
-          {
-            title: 'Title 3',
-            info: 'Info 3'
-          },
-          {
-            title: 'Title 4',
-            info: 'Info 4'
-          }
-    ]
-
-    return (
-        <List
-            itemLayout="horizontal"
-            dataSource={dummy}
-            renderItem={item => (
-                <List.Item>
-                  <List.Item.Meta
-                    title={<a href="https://ant.design">{item.title}</a>}
-                    description={item.info}
-                  />
-                </List.Item>
-              )}
-        />
-    )
+const TransactionsList = ({ data }) => {
+  if (Array.isArray(data)) {
+    data.sort((a, b) => {
+      return new Date(b?.bankStatement?.date) - new Date(a?.bankStatement?.date)
+    })
+  }
+  return (
+    <div
+      id="scrollableDiv"
+      style={{
+        height: 400,
+        overflow: 'auto'
+      }}
+    >
+      <List
+        itemLayout="horizontal"
+        dataSource={data}
+        renderItem={item => {
+          const { bankStatement } = item || {}
+          const { date, name: orderName, amount } = bankStatement || {}
+          return <List.Item className="ant-list-item-no-flex">
+            <Row>
+              <Col span={4}>{date}</Col>
+              <Col span={4}>{orderName}</Col>
+              <Col style={{ textAlign: "right", color: amount > 0 ? "green" : "red" }} span={16}>{amount}</Col>
+            </Row>
+          </List.Item>
+        }}
+      />
+    </div >
+  )
 }
 
 export default TransactionsList
