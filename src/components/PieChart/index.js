decdb6e9c5e8008386bf09675acb9810117cc71c
Diana Tungatarov <dianatungatarov@MBP-Diana.fritz.box>
2022-01-21 19:57:07 +0100

    rewrite pie chart logic

--- src/components/PieChart/index.js
+++ src/components/PieChart/index.js
@@ -5,26 +5,46 @@ import './index.css'
 
 const PieChart = ({ data: planeData }) => {
   const [selected, setSelected] = useState(null)
-  console.log('selected', selected)
 
   let data = []
+  let outcome = []
+  let income = []
+
+  console.log('data', data)
   const planeCategories = planeData.map(i => i.category)
   const uniqueCategories = [...new Set(planeCategories)]
 
-  uniqueCategories.map(i => data.push({ type: i, value: 0, count: 0 }))
+  uniqueCategories.map(i => data.push({ type: i, value: 0, outcomeSum: 0, incomeSum: 0 }))
 
-  data.forEach(chartData => {
-    const transactionsQuantity = Object.keys(planeData).length
-
-    planeData.forEach(transaction => (transaction.category === chartData.type)
-      ? chartData.count = chartData.count + 1
-      : null
-    )
-
-    chartData.value = 100 * chartData.count / transactionsQuantity
+  planeData.forEach(item => {
+    (item.bankStatement.amount < 0)
+      ? outcome.push(item.bankStatement.amount)
+      : income.push(item.bankStatement.amount)
   })
 
+  const negativeOutcomeSum = outcome.reduce((a, b) => (a + b), 0)
+  const outcomeSum = Math.abs(negativeOutcomeSum)
 
+  const increaseChartDataSumState = (amount, status, chartData) => {
+    const convertedAmount = Math.abs(amount)
+
+    if (status === 'outcome') {
+      return chartData.outcomeSum = chartData.outcomeSum + convertedAmount
+    }
+    return chartData.incomeSum = chartData.incomeSum + amount
+  }
+
+  data.forEach(chartData => {
+    planeData.forEach(transaction => {
+      if (transaction.category === chartData.type) {
+        (transaction.bankStatement.amount < 0)
+          ? increaseChartDataSumState(transaction.bankStatement.amount, 'outcome', chartData)
+          : increaseChartDataSumState(transaction.bankStatement.amount, 'income', chartData)
+      }
+    })
+
+    chartData.value = 100 * chartData.outcomeSum / outcomeSum
+  })
 
   const config = {
     appendPadding: 10,
