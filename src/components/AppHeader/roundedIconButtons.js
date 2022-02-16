d7e6ca3f5fda3be130f1f916d73182b31c215fb7
Diana Tungatarov <dianatungatarov@MBP-Diana.fritz.box>
2022-02-16 03:49:30 +0100

    add modals

--- src/components/AppHeader/roundedIconButtons.js
+++ src/components/AppHeader/roundedIconButtons.js
@@ -1,25 +1,40 @@
+import React, { useState } from 'react'
+import QuickStart from '../QuickStart'
+import ProductInfo from '../ProductInfo'
+import ProductDoc from '../ProductDoc'
+
 const RoundedIconButtons = () => {
-  const IconLink = ({ src, text }) => (
-    <a className='example-link'>
+  const [isQuickStartActive, setQuickStartActive] = useState(false)
+  const [isProductInfoActive, setProductInfoActive] = useState(false)
+  const [isProductDocActive, setProductDocActive] = useState(false)
+
+  const IconLink = ({ src, text, handleClick }) => (
+    <a className='example-link' onClick={handleClick}>
       <img className='example-link-icon' src={src} alt={text} />
       {text}
     </a>
-  );
+  )
 
   return (
     <div className='roundedButtons'>
       <IconLink
         src='https://gw.alipayobjects.com/zos/rmsportal/MjEImQtenlyueSmVEfUD.svg'
         text=' Quick Start'
+        handleClick={() => setQuickStartActive(true)}
       />
       <IconLink
         src='https://gw.alipayobjects.com/zos/rmsportal/NbuDUAuBlIApFuDvWiND.svg'
         text=' Product Info'
+        handleClick={() => setProductInfoActive(true)}
       />
       <IconLink
         src='https://gw.alipayobjects.com/zos/rmsportal/ohOEPSYdDTNnyMbGuyLb.svg'
         text=' Product Doc'
+        handleClick={() => setProductDocActive(true)}
       />
+      <QuickStart isModalVisible={isQuickStartActive} setIsModalVisible={setQuickStartActive} />
+      <ProductInfo isModalVisible={isProductInfoActive} setIsModalVisible={setProductInfoActive} />
+      <ProductDoc isModalVisible={isProductDocActive} setIsModalVisible={setProductDocActive} />
     </div>
   )
 }
---
+++ src/components/ProductDoc/index.js
@@ -0,0 +1,26 @@
+import { Modal, Button } from 'antd'
+
+const ProductDoc = ({ isModalVisible, setIsModalVisible }) => {
+  const handleOk = () => {
+    setIsModalVisible(false)
+  }
+
+  return (
+    <Modal
+      title="Basic Modal"
+      visible={isModalVisible}
+      closable={false}
+      footer={
+        <Button key="submit" type="primary" onClick={handleOk}>
+          Ok
+        </Button>
+      }
+    >
+      <p>Some contents...</p>
+      <p>Some contents...</p>
+      <p>Some contents...</p>
+    </Modal>
+  )
+}
+
+export default ProductDoc
