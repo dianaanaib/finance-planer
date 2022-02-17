2da88077910e9b98d0312f2b0266d55ac0eff3ba
Diana Tungatarov <dianatungatarov@MacBook-Pro-Diana.fritz.box>
2022-02-17 18:31:34 +0100

    adjust/separate hover

--- src/components/AppHeader/chartToolbar.js
+++ src/components/AppHeader/chartToolbar.js
@@ -2,16 +2,16 @@ import { FallOutlined, FundViewOutlined, RiseOutlined } from '@ant-design/icons'
 
 const ChartToolbar = ({ chartType, setChartType }) => {
 
-    return (
-        <div className='chartToolbar'>
-            {
-                (chartType === 'outcome')
-                    ? <FallOutlined style={{ fontSize: '20px', color: '#1890ff' }} onClick={() => setChartType('income')} />
-                    : <RiseOutlined style={{ fontSize: '20px', color: '#4cb68b' }} onClick={() => setChartType('outcome')} />
-            }
-            <FundViewOutlined style={{ fontSize: '20px' }} />
-        </div>
-    )
+	return (
+		<div className='chartToolbar'>
+			{
+				(chartType === 'outcome')
+					? <FallOutlined className='fallOutlined' style={{ fontSize: '20px', color: '#1890ff' }} onClick={() => setChartType('income')} />
+					: <RiseOutlined className='riseOutlined' style={{ fontSize: '20px', color: '#4cb68b' }} onClick={() => setChartType('outcome')} />
+			}
+			<FundViewOutlined className='fundViewOutlined' style={{ fontSize: '20px' }} />
+		</div>
+	)
 }
 
 export default ChartToolbar
