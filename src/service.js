53fd997c23f167c3c4244ab577f477fa4005d5f5
Diana Tungatarov <dianatungatarov@MacBook-Pro-Diana.fritz.box>
2022-02-17 20:15:30 +0100

    implement files service

--- src/service.js
+++ src/service.js
@@ -1,4 +1,18 @@
 export async function getAllBankStatements() {
-    const response = await fetch('/api/v1/data', { headers: { 'accept': 'application/json' } })
+    const response = await fetch('/api/v1/data', {
+        headers: {
+            'accept': 'application/json',
+        }
+    })
+    return response.json()
+}
+
+export async function getAllUploadedFiles() {
+    const response = await fetch('/api/v1/files', {
+        headers: {
+            'accept': 'application/json',
+        }
+    })
+    console.log('res', response)
     return response.json()
 }
